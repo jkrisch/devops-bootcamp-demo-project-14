a25def627e6ed78cc1c18c03678ccbeb60681fbb
jonas <jonas.krisch@gmail.com>
2024-10-21 11:12:06 +0200

    adding scheduling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import boto3
+import schedule
 
 ec2_client = boto3.client('ec2', region_name = "eu-central-1")
 ec2_resource = boto3.resource('ec2', region_name = "eu-central-1")
@@ -10,12 +11,20 @@ instances = ec2_client.describe_instances()
 # Reservation represents startin/launcing multiple instances
 # One reservation is an act of launching/starting instances so we might have multiple reservations and each reservations might have multiple instances
 
-statuses = status = ec2_client.describe_instance_status()
 
-for status in statuses['InstanceStatuses']:
-    ins_status = status['InstanceStatus']['Status']
-    sys_status = status['SystemStatus']['Status']
-    state = status['InstanceState']['Name']
-    print(f"Instance {status['InstanceId']} is {state} with instance status {ins_status} and system status {sys_status}")
+
+def check_ec2_instance_status():
+    statuses = ec2_client.describe_instance_status(IncludeAllInstances=True)
+
+    for status in statuses['InstanceStatuses']:
+        ins_status = status['InstanceStatus']['Status']
+        sys_status = status['SystemStatus']['Status']
+        state = status['InstanceState']['Name']
+        print(f"Instance {status['InstanceId']} is {state} with instance status {ins_status} and system status {sys_status}")
         
+    print("################################################")
 
+schedule.every(10).seconds.do(check_ec2_instance_status)
+
+while(True):
+    schedule.run_pending()
