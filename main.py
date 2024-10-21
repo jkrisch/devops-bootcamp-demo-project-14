b3fccea9b07dfc71fe0fe70ec256574f30d29e96
jonas <jonas.krisch@gmail.com>
2024-10-21 11:38:56 +0200

    adding configure script

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 import boto3
-import schedule
 
-ec2_client = boto3.client('ec2', region_name = "eu-central-1")
-ec2_resource = boto3.resource('ec2', region_name = "eu-central-1")
+def add_tag_to_ec2_instances(region, tag):
+    ec2_client = boto3.client('ec2', region_name=region)
+    ec2_resource = boto3.resource ('ec2', region_name=region)
 
-#retrieve all instances in the specified region
-instances = ec2_client.describe_instances()
+    #retrieve all instances in the specified region
+    instances = ec2_client.describe_instances()['Reservations']
 
-# What we get is a list of Reservations
-# Reservation represents startin/launcing multiple instances
-# One reservation is an act of launching/starting instances so we might have multiple reservations and each reservations might have multiple instances
+    instances_ids = []
+    if instances:
+        for res in instances:
+            instances = res['Instances']
+            for instance in instances:
+                instances_ids.append(instance["InstanceId"])
 
 
+        response = ec2_resource.create_tags(
+            Resources =  instances_ids,
+            Tags = [
+                tag
+            ]
+        )
 
-def check_ec2_instance_status():
-    statuses = ec2_client.describe_instance_status(IncludeAllInstances=True)
+#adding prod to frankfurt
+region = "eu-central-1"
+tag = {
+    "Key":"environment",
+    "Value": "prod"
+}
+add_tag_to_ec2_instances(region=region, tag=tag)
 
-    for status in statuses['InstanceStatuses']:
-        ins_status = status['InstanceStatus']['Status']
-        sys_status = status['SystemStatus']['Status']
-        state = status['InstanceState']['Name']
-        print(f"Instance {status['InstanceId']} is {state} with instance status {ins_status} and system status {sys_status}")
-        
-    print("################################################")
-
-schedule.every(10).seconds.do(check_ec2_instance_status)
-
-while(True):
-    schedule.run_pending()
+#adding dev to paris
+region = "eu-west-3"
+tag = {
+    "Key":"environment",
+    "Value": "dev"
+}
+add_tag_to_ec2_instances(region=region, tag=tag)
