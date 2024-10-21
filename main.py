28f2e1c0000c13ea057364d1770b1725a9b6d1d5
jonas <jonas.krisch@gmail.com>
2024-10-21 16:03:52 +0200

    adding webserver monitor script

--- main.py
+++ main.py
@@ -1,30 +1,66 @@
-import boto3
+import requests
 import schedule
-
-ec2_client = boto3.client('ec2', region_name = "eu-central-1")
-ec2_resource = boto3.resource('ec2', region_name = "eu-central-1")
-
-#retrieve all instances in the specified region
-instances = ec2_client.describe_instances()
-
-# What we get is a list of Reservations
-# Reservation represents startin/launcing multiple instances
-# One reservation is an act of launching/starting instances so we might have multiple reservations and each reservations might have multiple instances
-
+import smtplib
+import paramiko
+import digitalocean as do
+import os
+import time
+import sys
+host_ip = "157.230.119.62"
+webserver_url = f"http://{host_ip}:8080/"
+DO_TOKEN = os.environ.get("DO_TOKEN")
 
 
-def check_ec2_instance_status():
-    statuses = ec2_client.describe_instance_status(IncludeAllInstances=True)
-
-    for status in statuses['InstanceStatuses']:
-        ins_status = status['InstanceStatus']['Status']
-        sys_status = status['SystemStatus']['Status']
-        state = status['InstanceState']['Name']
-        print(f"Instance {status['InstanceId']} is {state} with instance status {ins_status} and system status {sys_status}")
+def send_email_notification(msg):
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.starttls()
+        smtp.ehlo()
         
-    print("################################################")
+        #I stopped writing code for the sending email part but continued following the lecture. The issue is that I could not create an application password for my gmail account as this feature has been deprecated.
+        #Therefore oAuth was needed, and I struggled a little setting it up.
 
-schedule.every(10).seconds.do(check_ec2_instance_status)
+def restart_server_and_application():
+    manager = do.Manager(token=DO_TOKEN)
+    nginx_droplet = manager.get_droplet(droplet_id="453045946")
+    print("Rebooting droplet")
+    nginx_droplet.reboot()
+    
+    while (True):
+        time.sleep(10)
+        nginx_droplet = manager.get_droplet(droplet_id="453045946")
+        print(nginx_droplet.status)
+        if nginx_droplet.status == "active":
+            restart_application()
+            break
 
-while(True):
+
+#Restart application
+def restart_application():
+    #in case of a bad request response we restart the application by ssh-ing into the server and restarting the container
+    #we do this using the paramiko library
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=host_ip, username="root", key_filename="/home/jonas/.ssh/id_rsa")
+    stdin, stdout, stderr = ssh.exec_command("docker start c82a3959f7c3")
+    print(stdout.readlines())
+    ssh.close()
+
+def monitor_application():
+    try:
+        response = requests.get(webserver_url)
+
+        if response.status_code == 200:
+            print("Application is up and running.")
+
+        else:
+            message = "Application is not running!"
+            #send_email_notification(msg=message)
+            
+    except Exception as error:
+        #send_email_notification(msg=f"Connection error happened {error}")
+        restart_server_and_application()
+
+schedule.every(20).seconds.do(monitor_application)
+
+while True:
     schedule.run_pending()
